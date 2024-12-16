da9a012697ee14a847d26e2380cff6bf7b1ee39d
Zeno Jiricek <airtonix@users.noreply.github.com>
2024-12-16 23:35:50 +1030

    nav links

--- app/components/common/GlobalNav/GlobalNav.tsx
+++ app/components/common/GlobalNav/GlobalNav.tsx
@@ -31,10 +31,7 @@ const Styles = tv({
     },
     isAncestor: {
       true: {
-        link: [
-          'visited:bg-background-link-active visited:text-text-link-active [text-decoration: none]',
-          'visited:hover:bg-background-card visited:hover:text-text-positive visited:hover:[text-decoration: none]',
-        ],
+        link: [],
       },
     },
     useLogo: {
@@ -80,13 +77,12 @@ export function GlobalNav({
         >
           <Link
             href="/b"
-            className={classnames(
-              styles.link({ isAncestor: path.isAncestor('/b') })
-            )}
+            className={classnames(styles.link())}
+            current={path.isAncestor('/b')}
           >
             Posts
           </Link>
-        </LinkList>{' '}
+        </LinkList>
         <Box
           className={styles.sociallinks()}
           direction={direction}
--- app/components/ds/link/Link.tsx
+++ app/components/ds/link/Link.tsx
@@ -1,30 +1,42 @@
-import { tv } from 'tailwind-variants';
+import { tv, type VariantProps } from 'tailwind-variants';
 
 import { classnames } from '~/core/classnames';
 
 import { useLinkComponent } from './useLinkComponent';
 
 const Styles = tv({
-  base: [
-    'underline',
-    'text-text-link',
-    'hover:text-text-link-hover',
-    'active:text-text-link-active',
-    'visited:text-text-link-visited',
-  ],
+  base: ['underline'],
+  variants: {
+    current: {
+      true: [
+        'text-text-positive/80',
+        'hover:text-text-positive',
+        'visited:bg-background-link-active visited:text-text-positive [text-decoration: none]',
+        'visited:hover:bg-background-card visited:hover:text-text-positive visited:hover:[text-decoration: none]',
+      ],
+      false: [
+        'text-text-link',
+        'hover:text-text-link-hover',
+        'active:text-text-link-active',
+        'visited:text-text-link-visited',
+      ],
+    },
+  },
 });
 
 export function Link({
   children,
   href,
   className,
+  current,
 }: React.DetailedHTMLProps<
   React.AnchorHTMLAttributes<HTMLAnchorElement>,
   HTMLAnchorElement
->) {
+> &
+  VariantProps<typeof Styles>) {
   const LinkComponent = useLinkComponent();
   const isExternal = !!href && href.startsWith('http');
-  const styles = Styles();
+  const styles = Styles({ current });
   const externalProps = isExternal
     ? { target: '_blank', rel: 'noreferrer' }
     : {};
